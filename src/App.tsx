72991bcb948e88c296134b5dcbab5aca96a7b8df
ygmurrsimsek <ygmurrsimsek@icloud.com>
2025-05-11 18:25:23 +0300

    Verilerimize ulaştık.
    Verilerimizin bulunduğu dosyadan verileri import ettik .FlatList sayesinde bu verileri çektik.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import React from "react";
-import { View ,Text,SafeAreaView} from "react-native";
-
+import { View ,Text,SafeAreaView,FlatList} from "react-native";
+import Products from './data/Products.json';
+//ilk olarak FlatList ile verileri alalım
 const PatikaStore = () => {
-  
+  console.log(Products);
   return (
     <SafeAreaView>
-      <Text>patikaDev</Text>
+      <FlatList data={Products} 
+      renderItem={({ item }) => (
+      <Text>{item.title}</Text>
+      )}/>
+
     </SafeAreaView>
   );
 };
