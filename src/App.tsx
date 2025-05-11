856cea7cc8fd24e488dd5f9cac0fd73778c66a7e
ygmurrsimsek <ygmurrsimsek@icloud.com>
2025-05-11 18:06:56 +0300

    dosya yapısı tamamlandı.

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { View ,Text,SafeAreaView} from "react-native";
+
+const PatikaStore = () => {
+  
+  return (
+    <SafeAreaView>
+      <Text>patikaDev</Text>
+    </SafeAreaView>
+  );
+};
+
+export default PatikaStore;
